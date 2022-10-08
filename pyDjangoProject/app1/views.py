8bb2883625325971a6e1e26c53375ce760ebadf8
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-08 23:57:59 +0600

    update

--- pyDjangoProject/app1/views.py
+++ pyDjangoProject/app1/views.py
@@ -2,4 +2,5 @@ import imp
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def 
+def index(request):
+    return HttpResponse("1st django project")
