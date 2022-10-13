cee978d3e4ff8e0d044f42016ec54842d9c8f294
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-13 17:18:43 +0600

    change path

--- pyDjangoProject/app1/views.py
+++ pyDjangoProject/app1/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     my_dict = {'insert_me':"hello"}
-    return render(request,'index.html',context=my_dict)
+    return render(request,'First File/index.html',context=my_dict)
