2e09df3787571e716eb35e3b90acfc07b07f4892
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-13 17:53:36 +0600

    CREATE STATIC FOLDER

--- pyDjangoProject/pyDjangoProject/settings.py
+++ pyDjangoProject/pyDjangoProject/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 TEMPLATE_DIR = Path.joinpath(BASE_DIR,"templates")
+STATIC_DIR = Path.joinpath(BASE_DIR,"static")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -112,7 +113,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILE_DIR = [
+    STATIC_DIR,
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
