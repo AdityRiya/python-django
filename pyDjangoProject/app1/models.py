0c3cb89eff105d6a37b155bbcbe835b0602c7ba6
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-14 23:25:55 +0600

    date

--- pyDjangoProject/app1/models.py
+++ pyDjangoProject/app1/models.py
@@ -13,3 +13,9 @@ class Webpage(models.Model):
     url = models.URLField(unique=True)
     def __str__(self):
         return self.name
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage)
+    date = models.DateField()
+    
+    def __str__(self):
+        return str(self.date)
