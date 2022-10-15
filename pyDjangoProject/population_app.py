13a7b3cb3d2e127042456bb749abe4d3a1a93e0e
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-16 00:29:08 +0600

    Update population_app.py

--- pyDjangoProject/population_app.py
+++ pyDjangoProject/population_app.py
@@ -1,6 +1,11 @@
 import imp
+from operator import imod
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','app1.settings')
 import django
 django.setup()
 import random
+from app1.models import AccessRecord,Topic,Webpage
+from faker import Faker
+fakegen = Faker()
+topics=['Search','Social','Marketplace','News','Games']
